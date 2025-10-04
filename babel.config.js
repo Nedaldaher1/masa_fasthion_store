f79395b3d2f600107263447ac8da9907af45bd2b
Nedaldaher1 <progammerabood@gmail.com>
2025-10-04 16:01:22 +0300

    init project

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   let plugins = [];
 
+  plugins.push('expo-router/babel');
   plugins.push('react-native-reanimated/plugin');
 
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }], 
+      'nativewind/babel'
+    ],
     plugins,
   };
 };
